4ac510653f87007fdd1a4e6da9de96f4944837e3
kotdn <denis.samoylenko.ds@gmail.com>
2026-04-24 00:30:01 +0300

    Remove generated release artifacts and ignore local packages

--- release/RDP-Security-Suite-ZIP-PUBLIC/unblock-all-files.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$root = Split-Path -Parent $MyInvocation.MyCommand.Path
-Get-ChildItem -Path $root -Recurse -File | Unblock-File
-Write-Host 'All files in package were unblocked.'
